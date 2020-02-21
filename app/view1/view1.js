c4d296d0f198040ccb8ea4333dde2ec920da38c3
Jere2357 <jeremiah@pearlpay.io>
2020-02-21 14:16:52 +0800

    created read wallet

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,48 +11,21 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', ['$scope', 'forecast', function($scope, forecast) {
 
-  $scope.title = "hello world";
+  $scope.walletData = [];
 
-  $scope.loadForeCastData = () => {
-    console.log("Entering load forecast");
-    forecast.then((data) => {
-      console.log(data);
-    });
-    console.log("Exit load forecast");
-  };
+  $scope.items = forecast.then(({data}) => {
+    return data;
+  });
 
-  $scope.items = [
-      "Item1",
-      "Item2",
-      "Item3",
-      "Item4",
-  ];
-
-  $scope.showFlag = false;
-  $scope.toggleShowTable = () => {
-      $scope.showFlag = !$scope.showFlag;
-  };
-  $scope.inventories = [
-    {
-        name: "Piattos",
-        manufacturer: "Jack n Jill",
-        price: 20.0
-    },
-    {
-      name: "Alibaba",
-      manufacturer: "Jill",
-      price: 2.0
-    },
-    {
-      name: "Nova",
-      manufacturer: "Jack n Jill",
-      price: 10.0
-    }
-  ];
+  $scope.showTable = forecast.then(({data}) => {
+    $scope.walletData = data;
+    console.log($scope.walletData);
+    return $scope.walletData;
+  });
 }])
   .factory('forecast', ['$http', function ($http) {  // forecast name of the service
 
-  return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json')
+  return $http.get('http://localhost:8080/wallet/records')
       .then(successCallback, errorCallback);
 
   function successCallback(data) {
